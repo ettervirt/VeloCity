6ac3cb3152fa286f96968b0426cbcc173bd7cc2c
Minek6 <dominik.florek1146@gmail.com>
2026-04-23 22:14:55 +0200

    feat(backend): #2 delete TicketType (#4)
    * feat(backend): #2 delete TicketType
    
    * fix: correct path for delete

---
+++ VeloCityServer/VeloCity.Api/Features/Tickets/Commands/DeleteTicketType/DeleteTicketTypeHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using VeloCity.Api.Common.Exceptions;
+using VeloCity.Api.Models.Data;
+
+namespace VeloCity.Api.Features.Tickets.Commands.DeleteTicketType
+{
+    public class DeleteTicketTypeHandler(ApplicationDbContext context)
+    : IRequestHandler<DeleteTicketTypeCommand>
+    {
+        public async Task Handle(DeleteTicketTypeCommand request, CancellationToken ct)
+        {
+            var ticketType = await context.TicketTypes.FindAsync([request.Id], ct)
+                       ?? throw new NotFoundException("TicketType", request.Id);
+
+            if (!ticketType.IsActive)
+            {
+                throw new AppException("Ticket type already inactive", 400);
+            }
+
+            ticketType.IsActive = false;
+
+            await context.SaveChangesAsync(ct);
+        }
+    }
+}
