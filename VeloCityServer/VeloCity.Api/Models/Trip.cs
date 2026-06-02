15c96b93b90279090c63af22155a18b92c6ba83d
Piotr Bołoz <piotrboloz0@gmail.com>
2026-06-02 11:38:19 +0200

    feat(backend): add timetable/trip table

---
+++ VeloCityServer/VeloCity.Api/Models/Trip.cs
@@ -0,0 +1,15 @@
+namespace VeloCity.Api.Models;
+
+public class Trip
+{
+    public int Id { get; set; }
+    public int LineId { get; set; }
+    public int VehicleId { get; set; }
+    public int DriverId { get; set; }
+    public bool IsActive { get; set; }
+
+    public virtual Line Line { get; set; } = null!;
+    public virtual Vehicle Vehicle { get; set; } = null!;
+    public virtual User Driver { get; set; } = null!;
+    public virtual ICollection<Timetable> Timetables { get; set; } = new List<Timetable>();
+}
